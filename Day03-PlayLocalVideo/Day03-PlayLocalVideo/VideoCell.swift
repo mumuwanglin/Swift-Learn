c60cbdceccb7283d985b87e1337c3ba203b31319
mumu <873621881@qq.com>
2019-05-07 23:07:04 +0800

    Day03

---
+++ Day03-PlayLocalVideo/Day03-PlayLocalVideo/VideoCell.swift
@@ -0,0 +1,24 @@
+//
+//  VideoCell.swift
+//  Day03-PlayLocalVideo
+//
+//  Created by 王林 on 2019/5/7.
+//  Copyright © 2019 王林. All rights reserved.
+//
+
+import UIKit
+
+class VideoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Day03-PlayLocalVideo/Day03-PlayLocalVideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day03-PlayLocalVideo
+//
+//  Created by 王林 on 2019/5/7.
+//  Copyright © 2019 王林. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

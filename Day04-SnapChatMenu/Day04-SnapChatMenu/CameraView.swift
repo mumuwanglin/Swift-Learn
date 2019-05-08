bcbaab4b0ad7b458d2be0a0faae42edfa316af67
mumu <873621881@qq.com>
2019-05-08 22:41:27 +0800

    Day04

--- Day04-SnapChatMenu/Day04-SnapChatMenu/CameraView.swift
+++ Day04-SnapChatMenu/Day04-SnapChatMenu/CameraView.swift
@@ -7,24 +7,113 @@
 //
 
 import UIKit
-
+import AVFoundation
 class CameraView: UIViewController {
 
+    @IBOutlet weak var tempImageView: UIImageView!
+    @IBOutlet weak var cameraView: UIView!
+    
+    var captureSession: AVCaptureSession?
+    var stillImageOutput : AVCaptureStillImageOutput?
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        captureSession = AVCaptureSession()
+        captureSession?.sessionPreset = AVCaptureSession.Preset.hd1920x1080
+        let backCamera = AVCaptureDevice.devices(for: .video).first!
+        
+        var error: NSError?
+        var input: AVCaptureDeviceInput!
+        
+        do{
+            input = try AVCaptureDeviceInput(device: backCamera)
+        }catch let e as NSError {
+            error = e
+            input = nil
+        }
+        
+        if(error == nil && captureSession?.canAddInput(input) != nil) {
+            captureSession?.addInput(input)
+            
+            stillImageOutput = AVCaptureStillImageOutput()
+            stillImageOutput?.outputSettings = [AVVideoCodecKey : AVVideoCodecJPEG]
+            
+            if let stillImageOutputTemp = stillImageOutput {
+                if captureSession?.canAddOutput(stillImageOutputTemp) != nil {
+                    captureSession?.addOutput(stillImageOutputTemp)
+                    if let captureSessionTemp = captureSession {
+                        previewLayer = AVCaptureVideoPreviewLayer(session: captureSessionTemp)
+                        previewLayer?.videoGravity = AVLayerVideoGravity.resizeAspect
+                        // 已经废弃不用了
+                        // previewLayer?.videoGravity = AVLayerVideoGravityResizeAspect
+                        previewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+                        cameraView.layer.addSublayer(previewLayer!)
+                        captureSession?.startRunning()
+                    }
+                }
+            }
+        }
     }
-    */
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        previewLayer?.frame = cameraView.bounds
+    }    
+    
+    func didPressTakePhoto(){
+        
+        if let videoConnection = stillImageOutput?.connection(with: .video){
+            videoConnection.videoOrientation = AVCaptureVideoOrientation.portrait
+            stillImageOutput?.captureStillImageAsynchronously(from: videoConnection, completionHandler: {
+                (sampleBuffer, error) in
+                
+                if sampleBuffer != nil {
+                    
+                    
+                    let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer!)
+                    let dataProvider  = CGDataProvider(data: imageData! as CFData)
+                    let cgImageRef = CGImage(jpegDataProviderSource: dataProvider!, decode: nil, shouldInterpolate: true, intent: CGColorRenderingIntent.defaultIntent)
+                    
+                    let image = UIImage(cgImage: cgImageRef!, scale: 1.0, orientation: .right)
+                    
+                    self.tempImageView.image = image
+                    self.tempImageView.isHidden = false
+                    
+                }
+                
+                
+            })
+        }
+        
+        
+    }
+    
+    
+    var didTakePhoto = Bool()
+    
+    func didPressTakeAnother(){
+        if didTakePhoto == true{
+            tempImageView.isHidden = true
+            didTakePhoto = false
+            
+        }
+        else{
+            captureSession?.startRunning()
+            didTakePhoto = true
+            didPressTakePhoto()
+            
+        }
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        didPressTakeAnother()
+    }
 }
--- Day04-SnapChatMenu/Day04-SnapChatMenu/ViewController.swift
+++ Day04-SnapChatMenu/Day04-SnapChatMenu/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let screenWidth = UIScreen.main.bounds.width
+        let screenHeight = UIScreen.main.bounds.height
+        let leftView: LeftView = LeftView(nibName: "LeftView", bundle: nil)
+        let cameraView: CameraView = CameraView(nibName: "CameraView", bundle: nil)
+        let rightView: RightView = RightView(nibName: "RightView", bundle: nil)
+
+        let leftSubView: UIView! = leftView.view
+        let cameraSubView: UIView! = cameraView.view
+        let rightSubView: UIView! = rightView.view
+        
+        leftSubView.frame = CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight)
+        cameraSubView.frame = CGRect(x: screenWidth, y: 0, width: screenWidth, height: screenHeight)
+        rightSubView.frame = CGRect(x: 2 * screenWidth, y: 0, width: screenWidth, height: screenHeight)
+        
+        self.scrollView.addSubview(leftSubView)
+        self.scrollView.addSubview(rightSubView)
+        self.scrollView.addSubview(cameraSubView)
+        
+        self.scrollView.contentSize = CGSize(width: screenWidth * 3, height: screenHeight)
     }
 
 
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+     
 }
 

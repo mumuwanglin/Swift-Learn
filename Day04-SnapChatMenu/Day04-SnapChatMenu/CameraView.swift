be64d12af2fda5393dd7bf9e298d872e758ff435
mumu <873621881@qq.com>
2019-05-08 22:41:23 +0800

    Day04

---
+++ Day04-SnapChatMenu/Day04-SnapChatMenu/CameraView.swift
@@ -0,0 +1,30 @@
+//
+//  CameraView.swift
+//  Day04-SnapChatMenu
+//
+//  Created by 王林 on 2019/5/8.
+//  Copyright © 2019 王林. All rights reserved.
+//
+
+import UIKit
+
+class CameraView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Day04-SnapChatMenu/Day04-SnapChatMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day04-SnapChatMenu
+//
+//  Created by 王林 on 2019/5/8.
+//  Copyright © 2019 王林. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

400602f5bbf4f10a2534c9298d3bc5ea40dda37e
mumu <873621881@qq.com>
2019-05-14 22:41:03 +0800

    Day08

---
+++ Day08-RandomColorization/Day08-RandomColorization/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day08-RandomColorization
+//
+//  Created by 王林 on 2019/5/14.
+//  Copyright © 2019 王林. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
